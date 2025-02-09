9d62a0a83b8d2b1acef028f7a8a251c11b5632b8
bbakkomm <bbakkomm@gmail.com>
2025-02-10 04:03:59 +0900

    로그인 전 화면 임시 구현

--- front/src/components/CustomButton.tsx
+++ front/src/components/CustomButton.tsx
@@ -28,15 +28,14 @@ function CustomButton({
             style={({pressed}) => [
                 styles.container, 
                 styles[variant], 
-                styles[size], 
                 pressed ? styles[`${variant}Pressed`] : styles[variant],
                 inValid && styles.inValid
             ]} 
             {...props}
         >
-            <Text 
-                style={[styles.text, styles[`${variant}Text`]]}
-            >{label}</Text>
+            <View style={styles[size]}>
+                <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+            </View>
         </Pressable>
     )
 }
@@ -45,6 +44,7 @@ const styles = StyleSheet.create({
     container: {
         borderRadius: 3,
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     inValid: {
         opacity: 0.5,
@@ -52,18 +52,18 @@ const styles = StyleSheet.create({
 
     // variant
     filled: {
-        backgroundColor: colors.PURPLE_700,
+        backgroundColor: colors.PURPLE_500,
     },
     outlined: {
-        borderColor: colors.PURPLE_700,
+        borderColor: colors.PURPLE_500,
         borderWidth: 1,
     },
     // variantPressed
     filledPressed: {
-        backgroundColor: colors.PURPLE_500,
+        backgroundColor: colors.PURPLE_300,
     },
     outlinedPressed: {
-        borderColor: colors.PURPLE_700,
+        borderColor: colors.PURPLE_300,
         borderWidth: 1,
         opacity: 0.5,
     },
@@ -74,12 +74,14 @@ const styles = StyleSheet.create({
         paddingVertical: deviceHeight > 700 ? 15 : 10,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
     },
     medium: {
         width: '50%',
         paddingVertical: deviceHeight > 700 ? 12 : 8,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
     },
 
     text: {
@@ -91,7 +93,7 @@ const styles = StyleSheet.create({
         color: colors.WHITE,
     },
     outlinedText: {
-        color: colors.PURPLE_700,
+        color: colors.PURPLE_500,
     },
 });
 
--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Dimensions, SafeAreaView, StyleSheet, View} from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, View, Image} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigtor';
 import { authNavigations } from '../../constants';
@@ -12,22 +12,53 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
     return (
-        <SafeAreaView>
-            <View>
-                <CustomButton 
-                    label='로그인하기' 
-                    onPress={() => navigation.navigate(authNavigations.LOGIN)} 
-                />
-                <CustomButton 
-                    label='회원가입하기' 
-                    variant='outlined'
-                    onPress={() => navigation.navigate(authNavigations.SIGNUP)} 
-                />
+        <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <View style={styles.imageContainer}>
+                    <Image 
+                        resizeMode='contain'
+                        style={styles.image} 
+                        source={require('../../assets/logo.png')}
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <CustomButton 
+                        label='로그인하기' 
+                        onPress={() => navigation.navigate(authNavigations.LOGIN)} 
+                    />
+                    <CustomButton 
+                        label='회원가입하기' 
+                        variant='outlined'
+                        onPress={() => navigation.navigate(authNavigations.SIGNUP)} 
+                    />
+                </View>
             </View>
         </SafeAreaView>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#200C36'
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        margin: 20,
+    },
+    imageContainer: {
+        flex: 1.5,
+        width: Dimensions.get('screen').width / 1.2,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    buttonContainer: {
+        flex: 1,
+        gap: 10
+    },
+});
 
 export default AuthHomeScreen;
