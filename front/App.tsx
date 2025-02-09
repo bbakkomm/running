c0826b79255b0a0aeba44b196ff829aaded07fa7
bbakkomm <bbakkomm@gmail.com>
2025-02-09 23:50:50 +0900

    navigation 구축

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Button,
@@ -7,15 +8,20 @@ import {
   View,
   SafeAreaView,
 } from 'react-native';
+import AuthStackNavigtor from './src/navigation/AuthStackNavigtor';
 
-function App(): React.JSX.Element {
+function App() {
+  const [name, setName] = useState('');
+
+  const handleChangeInput = (text: string) => {
+    setName(text);
+    console.log(text);
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input}/>
-        <Text>텍스트</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <AuthStackNavigtor />
+    </NavigationContainer>
   );
 }
 
@@ -23,14 +29,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  inputContainer: {
-
-  },
   input: {
+    flex: 1,
     borderWidth: 2,
     borderColor: 'black',
+    width: 100,
+    height: 50,
   },
-  
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 
 export default App;
---
+++ front/src/screens/LoginScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+interface LoginScreenProps {
+
+}
+
+const LoginScreen = ({}: LoginScreenProps) => {
+  return (
+    <View>
+        <Text>로그인 스크린</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default LoginScreen;
